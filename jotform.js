a6b4376a82324473ab3fbaaa314e2accf7243a6a
Amin Nayel <49832793+aminnayel@users.noreply.github.com>
2024-07-17 22:48:04 +0300

    Create jotform.js

---
+++ jotform.js
@@ -0,0 +1,61 @@
+
+// function fireJotform() {
+    console.log(document.getElementById("userId").value)
+
+function myWidget() {
+
+var widget = document.getElementById("userId")
+
+
+
+// get widget parameters if any
+var params = JFCustomWidget.getWidgetSettings();
+
+// exposed functions
+this.init = init;
+this.getData = getData;
+
+
+
+// initialization of the widget
+function init(formData) {
+}
+
+
+
+
+// get widget data
+function getData() {
+
+    return {
+        valid: true,
+        value: widget.value
+
+    };
+}
+
+
+// $('#userId').on('change', function() {
+
+// generateButton.addEventListener('click', function () {
+    
+setInterval(() => {
+    JFCustomWidget.sendData(getData());
+   
+    console.log("pushed to jotform");
+}, 500);
+
+}
+// )}
+
+JFCustomWidget.subscribe("ready", function(data) {
+var widget = new myWidget();
+widget.init(data);
+
+JFCustomWidget.subscribe("submit", function() {
+    JFCustomWidget.sendSubmit(widget.getData());
+});
+
+
+});
+
